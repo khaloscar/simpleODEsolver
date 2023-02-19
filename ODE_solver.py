8a00bfe057929679112e1b4ff75794b6fe0c31e2
khaloscar <khaloscar.se@gmail.com>
2023-02-19 21:44:09 +0100

    Update ODE_solver.py
    - Added some methods of order 1,2
        * doesnt really do much since order or precision is the same as RK2.
    But perhaps their area of stability is different?? Will have to look into that..
    - Other minor changes

--- ODE_solver.py
+++ ODE_solver.py
@@ -23,6 +23,35 @@ import matplotlib.pyplot as plt
 
 ## Numerical Methods
 
+def trapz(fun, tspan, u0, stepsize, *args):
+    # Trapezoidal method
+    N = int((tspan[-1] - tspan[0])/stepsize)
+    tt = np.linspace(tspan[0], tspan[1], N+1)
+    yy = np.array([u0])
+
+    for i in range(N):
+        tn = i*stepsize
+        w1 = fun(tn, yy[-1], *args)
+        w2 = fun(tn + stepsize, yy[-1] + stepsize*w1, *args)
+        ynext = yy[-1] + w1 -w2
+        yy = np.append(yy, [ynext], axis=0)
+    return tt, yy
+
+# Leap frog
+def leap_frog(fun, tspan, u0, stepsize, *args):
+    # Leap frog method
+
+    N = int((tspan[-1] - tspan[0])/stepsize)
+    tt = np.linspace(tspan[0], tspan[1], N+1)
+    yy = np.array([u0])
+
+    for i in range(N):
+        tn = i*stepsize
+        w1 = fun(tn, yy[-1], *args)
+        ynext = yy[-1] + 2*stepsize*w1
+        yy = np.append(yy, [ynext], axis=0)
+    return tt, yy
+
 def RK1(fun, tspan, u0, stepsize, *args):
     # Aka Eulers forward method
 
@@ -31,9 +60,23 @@ def RK1(fun, tspan, u0, stepsize, *args):
     yy = np.array([u0])
     tn = 0
     for i in range(N):
+        tn = i*stepsize
         ynext = yy[-1] + stepsize*fun(tn, yy[-1], *args)
         yy = np.append(yy, [ynext], axis=0)
-        tn += stepsize
+    return tt, yy
+
+def euler_bw(fun, tspan, u0, stepsize, *args):
+    # Euler backwards... For whatever reason
+    # Not sure if implemented correctly....
+
+    N = int((tspan[-1] - tspan[0])/stepsize)
+    tt = np.linspace(tspan[0], tspan[1], N+1)
+    yy = np.array([u0])
+
+    for i in range(N):
+        tn = i*stepsize
+        yy = np.append(yy, [yy[-1] - stepsize*fun(tn, yy[-1], *args)], axis=0)
+
     return tt, yy
 
 def RK2(fun, tspan, u0, stepsize, *args):
@@ -43,11 +86,11 @@ def RK2(fun, tspan, u0, stepsize, *args):
     yy = np.array([u0])
     tn = 0
     for i in range(N):
+        tn = i*stepsize
         F1 = fun(tn, yy[-1], *args)
         F2 = fun(tn + stepsize, yy[-1] + stepsize*F1, *args)
         ynext = yy[-1] + stepsize/2 * (F1 + F2)
         yy = np.append(yy, [ynext], axis=0)
-        tn += stepsize
     return tt, yy
 
 def RK4(fun, tspan, u0, stepsize, *args):
@@ -56,13 +99,13 @@ def RK4(fun, tspan, u0, stepsize, *args):
     yy = np.array([u0])
     tn = 0
     for i in range(N):
+        tn = i*stepsize
         F1 = fun(tn, yy[-1], *args)
         F2 = fun(tn + stepsize/2, yy[-1] + stepsize*F1/2, *args)
         F3 = fun(tn + stepsize/2, yy[-1] + stepsize*F2/2, *args)
         F4 = fun(tn + stepsize, yy[-1] + stepsize*F2, *args)
         ynext = yy[-1] + stepsize/6 * (F1 + 2*F2 + 2*F3 + F4)
         yy = np.append(yy, [ynext], axis=0)
-        tn += stepsize
     return tt, yy    
 
 
@@ -75,13 +118,15 @@ def testfun(t, y):
     return -y
 
 # The function for testing the implemented methods
-def tester(method = RK1):
+def tester(method = RK4):
     tspan = (0,4)
     u0 = 1
-    k = 0.1
+    k = 0.01
     yVal, xVal = method(testfun, tspan, u0, k)
     plt.plot(yVal, xVal)
     plt.title(f'Testing method {method}')
     plt.show()
 
-tester(RK4)
+
+if __name__ == "__main__":
+    tester(euler_bw)
